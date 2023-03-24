37c7abaf1b619206ff1d938a5967abf7f308b5e6
TexTrue <3140846162@qq.com>
2023-03-24 15:50:36 +0800

    add mod url

--- README.md
+++
@@ -1,7 +0,0 @@
-# Better Rubidium Video Settings
-
-Better Sodium Video Settings Forge Port (Unofficial)
-
-This mod changes back the 'Video Options' button to it's original state. 
-To access the sodium Video settings go to the Video settings and press sodium-ForgePort video settings.
-
---
+++ src/main/java/com/limeshulkerbox/bsvsb/BSVSBForge.java
@@ -0,0 +1,37 @@
+package com.limeshulkerbox.bsvsb;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(BSVSBForge.MODID)
+public class BSVSBForge {
+
+    public static final String MODID = "bsvsb";
+    public static Logger LOGGER = LogManager.getLogger("BRVSB");
+
+    public static boolean rubidiumLoaded;
+    public static boolean TROLoaded;
+
+    public BSVSBForge() {
+        IEventBus MOD_BUS = FMLJavaModLoadingContext.get().getModEventBus();
+        MOD_BUS.addListener(this::onInitializeClient);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    public void onInitializeClient(final FMLClientSetupEvent event) {
+        TROLoaded = ModList.get().isLoaded("reeses_sodium_options");
+        rubidiumLoaded = ModList.get().isLoaded("rubidium");
+        if (rubidiumLoaded) {
+            LOGGER.info("Rubidium is Loaded!");
+            if (TROLoaded) {
+                LOGGER.info("TexTrue's Rubidium Options is Loaded!");
+            }
+        }
+    }
+}
