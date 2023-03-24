030009b8ac666ee45924de1258407208ddd7235b
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-03-24 10:13:46 +0800

    Try fix issue TexTrueStudio/BetterSodiumVideoSettings-Forge#6

--- src/main/java/com/limeshulkerbox/bsvsb/BRVSB.java
+++ src/main/java/com/limeshulkerbox/bsvsb/BRVSB.java
@@ -1,14 +1,12 @@
 package com.limeshulkerbox.bsvsb;
 
 import net.minecraftforge.fml.ExtensionPoint;
-//import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
-//import net.minecraftforge.network.NetworkConstants;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
--- src/main/java/com/limeshulkerbox/bsvsb/mixin/MixinVideoOptionsScreen.java
+++ src/main/java/com/limeshulkerbox/bsvsb/mixin/MixinVideoOptionsScreen.java
@@ -22,75 +22,30 @@ public class MixinVideoOptionsScreen extends Screen {
     @Final
     @Mutable
     private static Option[] OPTIONS;
-    @Unique
-    Constructor<?> SodiumVideoOptionsScreenClassCtor;
-    @Unique
-    Constructor<?> SodiumOptionsGUIClassCtor;
     protected MixinVideoOptionsScreen(Text title) {
         super(title);
     }
 
-    @Inject(method = "<clinit>", at = @At("TAIL"))
-    private static void removeOptions(CallbackInfo ci) {
-        OPTIONS = ArrayUtils.removeElement(OPTIONS, Option.FULLSCREEN);
-        OPTIONS = ArrayUtils.removeElement(OPTIONS, Option.CLOUDS);
-    }
-
     @Inject(method = "init", at = @At("HEAD"))
     void mixinInit(CallbackInfo callbackInfo) {
-        this.addButton(new ButtonWidget(this.width / 2 + 5, this.height - 27, 150, 20, new TranslatableText("text.bettersodiumvideosettings.sodiumvideosettings"), (button) -> {
-            if (TROLoaded) {
-                flashyReesesOptionsScreen();
-            } else {
-                sodiumVideoOptionsScreen();
-            }
+        this.addButton(new ButtonWidget(this.width / 2 + 5, this.height - 27, 150, 20, new TranslatableText("button.bsvsb.video_settings_button"), (button) -> {
+            assert this.client != null;
+            this.client.openScreen(new SodiumOptionsGUI(this));
         }));
     }
-
-    @Unique
-    void flashyReesesOptionsScreen() {
-        if (SodiumVideoOptionsScreenClassCtor == null) {
-            try {
-                SodiumVideoOptionsScreenClassCtor = Class.forName("me.flashyreese.mods.reeses_sodium_options.client.gui.SodiumVideoOptionsScreen").getConstructor(Screen.class);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        try {
-            assert this.client != null;
-            this.client.openScreen((Screen) SodiumVideoOptionsScreenClassCtor.newInstance(this));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Unique
-    void sodiumVideoOptionsScreen() {
-        if (SodiumOptionsGUIClassCtor == null) {
-            try {
-                SodiumOptionsGUIClassCtor = Class.forName("me.jellysquid.mods.sodium.client.gui.SodiumOptionsGUI").getConstructor(Screen.class);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        try {
-            assert this.client != null;
-            this.client.openScreen((Screen) SodiumOptionsGUIClassCtor.newInstance(this));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonListWidget;addSingleOptionEntry(Lnet/minecraft/client/option/Option;)I", ordinal = 0))
+    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonListWidget;addSingleOptionEntry(Lnet/minecraft/client/options/Option;)I", ordinal = 0))
     private int removeGraphicsButton(ButtonListWidget buttonListWidget, Option option) {
         return 0;
     }
-
+    @Inject(method = "<clinit>", at = @At("TAIL"))
+    private static void removingFullscreenResolution(CallbackInfo ci) {
+        OPTIONS = ArrayUtils.removeElement(OPTIONS, Option.FULLSCREEN);
+        OPTIONS = ArrayUtils.removeElement(OPTIONS, Option.CLOUDS);
+    }
     @ModifyConstant(method = "init", constant = @Constant(intValue = 100, ordinal = 0))
     private int modifyDonePos(int input) {
         return 155;
     }
-
     @ModifyConstant(method = "init", constant = @Constant(intValue = 200, ordinal = 0))
     private int modifyDoneWidth(int input) {
         return 150;
